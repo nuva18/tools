b3c30963df403caeebec2292e8195c1aea310c3b
nuva18 <nuva18@users.noreply.github.com>
2024-10-21 21:13:11 -0400

    Added keyme.

---
+++ sh/keyme.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "usage: keyme <filename>" 
+    echo "       Creates a new ssh key." 
+    exit 1
+fi
+
+ssh-keygen -t ed25519 -b 4096 -f /home/$USER/.ssh/$1
+
+echo
+echo "Done. Here's your public key:"
+cat /home/$USER/.ssh/$1.pub
+echo
+exit 0
