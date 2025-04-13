e6216b9c72cc1bc72d0a159063b823a8168c8bd8
nuva18 <185148565+nuva18@users.noreply.github.com>
2025-04-13 19:57:36 -0400

    randnames can take seeds now

--- randnames/randnames.cpp
+++ randnames/randnames.cpp
@@ -5,6 +5,10 @@
 #include "names.hpp"
 using namespace std;
 
+/* 4/13/25: at some point this got mangled, i guess as a result of my days using geany. i feel 
+ * inclined to fix it, but i've only just had any need to update this and i don't have time 
+ * right now. this probably means anything else i wrote before adopting neovim is scuffed. */
+
 /* Return a random element from a vector. */
 string get_random_from_vector(const vector <string> &vec) {
     return vec[lrand48() % vec.size()];
@@ -60,7 +64,7 @@ string gen_name(char preferred_gender) {
 
 /* Print and error and kill the program. */
 void fatal_error(const string &error) {
-    fprintf(stderr, "usage: randnames [no-names] [gender (m|f|x)]\n");
+    fprintf(stderr, "usage: randnames [no-names] [seed] [gender (m|f|x)]\n");
     if (error == "") fprintf(stderr, "       Generates random names.\n");
     else fprintf(stderr, "%s\n", error.c_str());
     
@@ -70,27 +74,30 @@ void fatal_error(const string &error) {
 int main(int argc, char **argv) {
     unordered_set <string> used;
     string current_name;
-    int i, no_names;
+    int i, no_names, seed;
     char preferred_gender;
-
-    srand48(time(NULL));
 
     no_names = 1;
     preferred_gender = 'x';
+    seed = -1;
 
     /* Command line variables. 
-       randnames no-names gender */
+       randnames no-names seed gender */
     
     if (argc >= 2) {
 		if (argv[1][0] == '?') fatal_error("");
 		if (sscanf(argv[1], "%d", &no_names) != 1) fatal_error("Bad no-names.");
+		if (argc >= 3 && sscanf(argv[2], "%d", &seed) != 1) fatal_error("Bad seed.");
 	}
 	else no_names = 1;
 	
 	if (argc == 3) preferred_gender = argv[2][0];
 	else preferred_gender = 'x';
     
-	/* Generate and print the names. */
+    if (seed == -1) srand48(time(NULL));
+	  else srand48(seed);
+
+    /* Generate and print the names. */
 
     for (i = 0; i < no_names; i++) {
         do {
