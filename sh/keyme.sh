858f51d03dd122da4c80bfb7f4ee5198d4b3be9f
nuva18 <nuva18@users.noreply.github.com>
2025-08-30 15:12:58 -0400

    tried harder to fix keyme not working on systems with a weird home directory

--- sh/keyme.sh
+++ sh/keyme.sh
@@ -6,9 +6,12 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-ssh-keygen -t ed25519 -b 4096 -f /home/$USER/.ssh/$1
+priv_key=$HOME/.ssh/$1
+pub_key=$priv_key.pub
+
+ssh-keygen -t ed25519 -b 4096 -f $priv_key 
 
 echo
 echo "Done. Here's your public key:"
-cat $HOME/.ssh/$1.pub
+cat $pub_key
 exit 0
